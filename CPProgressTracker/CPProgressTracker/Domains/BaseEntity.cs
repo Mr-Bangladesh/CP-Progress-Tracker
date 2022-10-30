b6f1508b2e08c4ff7f5616c5b13f144b1356ab2c
Faizul_Haque <faizul.haque@brainstation-23.com>
2022-10-31 01:13:24 +0600

    added basic folders

---
+++ CPProgressTracker/CPProgressTracker/Domains/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace CPProgressTracker.Domains
+{
+    public class BaseEntity
+    {
+        public virtual int Id { get; set; }
+    }
+}
---
+++ CPProgressTracker/CPProgressTracker/Models/BaseEntityModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CPProgressTracker.Models
+{
+    public class BaseEntityModel
+    {
+        public virtual int Id { get; set; }
+    }
+}
