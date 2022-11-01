e87d160c7522a4822521e74cd44dcd218bb34141
Faizul_Haque <faizul.haque@brainstation-23.com>
2022-11-01 23:48:55 +0600

    working on object context

---
+++ CPProgressTracker/CP.Data/CPObjectContext.cs
@@ -0,0 +1,66 @@
+﻿using CP.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.Data
+{
+    /// <summary>
+    /// Represents base object context
+    /// </summary>
+    public class CPObjectContext : DbContext, IDbContext
+    {
+        #region Ctor
+
+        public CPObjectContext(DbContextOptions<CPObjectContext> options) : base(options)
+        {
+        }
+
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Further configuration the model
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //dynamically load all entity and query type configurations
+            var typeConfigurations = Assembly.GetExecutingAssembly().GetTypes().Where(type =>
+                (type.BaseType?.IsGenericType ?? false)
+                    && (type.BaseType.GetGenericTypeDefinition() == typeof(NopEntityTypeConfiguration<>)
+                        || type.BaseType.GetGenericTypeDefinition() == typeof(NopQueryTypeConfiguration<>)));
+
+            foreach (var typeConfiguration in typeConfigurations)
+            {
+                var configuration = (IMappingConfiguration)Activator.CreateInstance(typeConfiguration);
+                configuration.ApplyConfiguration(modelBuilder);
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Creates a DbSet that can be used to query and save instances of entity
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <returns>A set for the given entity type</returns>
+        public virtual new DbSet<TEntity> Set<TEntity>() where TEntity : BaseEntity
+        {
+            return base.Set<TEntity>();
+        }
+
+        #endregion
+    }
+}
