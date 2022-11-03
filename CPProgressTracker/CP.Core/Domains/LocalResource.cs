e384404de806fd38c58713d482209b32ba037fc6
Faizul_Haque <faizul.haque@brainstation-23.com>
2022-11-03 16:11:06 +0600

    added local resource entity

---
+++ CPProgressTracker/CP.Core/Domains/LocalResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.Core.Domains
+{
+    public class LocalResource : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the string resource key
+        /// </summary>
+        public string ResourceKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the string resource value
+        /// </summary>
+        public string ResourceValue { get; set; }
+    }
+}
