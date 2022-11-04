f26e2fc502976591260b0f5a42076c4b0f5b2eb1
Faizul_Haque <faizul.haque@brainstation-23.com>
2022-11-04 21:24:46 +0600

    worked on service layer

---
+++ CPProgressTracker/CP.Services/IConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.Services
+{
+    /// <summary>
+    /// Consumer interface
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    public interface IConsumer<T>
+    {
+        /// <summary>
+        /// Handle event
+        /// </summary>
+        /// <param name="eventMessage">Event</param>
+        void HandleEvent(T eventMessage);
+    }
+}
---
+++ CPProgressTracker/CP.Services/IEventPublisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.Services
+{
+    /// <summary>
+    /// Represents an event publisher
+    /// </summary>
+    public interface IEventPublisher
+    {
+        /// <summary>
+        /// Publish event to consumers
+        /// </summary>
+        /// <typeparam name="TEvent">Type of event</typeparam>
+        /// <param name="event">Event object</param>
+        void Publish<TEvent>(TEvent @event);
+    }
+}
